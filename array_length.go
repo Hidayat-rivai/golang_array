59abfe558d24da31ecb8125e7ab08977f284bb71
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-12-21 09:47:57 +0700

    array

---
+++ array_length.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var buah = [3] string {"apple","mangga", "jeruk"}
+	fmt.Println(len(buah))
+	
+}
---
+++ get_value.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var buah = [3] string {"apple","mangga", "jeruk"}
+	fmt.Println(buah[1])
+	
+}
